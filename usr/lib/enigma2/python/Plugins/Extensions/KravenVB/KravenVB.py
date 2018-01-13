56f10b3782c02dce0140a60da2001c25d5dcd947
örlgrey <ochzoetna@gmail.com>
2018-01-13 23:34:00 +0100

    Update to Version 6.4.22

--- usr/lib/enigma2/python/Plugins/Extensions/KravenVB/KravenVB.py
+++ usr/lib/enigma2/python/Plugins/Extensions/KravenVB/KravenVB.py
@@ -745,11 +745,6 @@ config.plugins.KravenVB.RunningTextSpeed = ConfigSelection(default="steptime=100
 				("steptime=50", _("20 px/sec"))
 				])
 
-config.plugins.KravenVB.RunningTextRenderer = ConfigSelection(default="Kraven", choices = [
-				("vti", _("VTi")),
-				("kraven", _("Kraven"))
-				])
-
 config.plugins.KravenVB.ScrollBar = ConfigSelection(default="scrollbarWidth=0", choices = [
 				("scrollbarWidth=0", _("off")),
 				("scrollbarWidth=5", _("thin")),
@@ -1202,7 +1197,7 @@ class KravenVB(ConfigListScreen, Screen):
   </widget>
   <widget backgroundColor="#00000000" name="config" font="Regular;22" foregroundColor="#00ffffff" itemHeight="30" position="70,85" size="708,540" enableWrapAround="1" scrollbarMode="showOnDemand" transparent="1" zPosition="1" />
   <eLabel backgroundColor="#00000000" text="KravenVB" font="Regular;36" foregroundColor="#00f0a30a" position="830,80" size="402,46" halign="center" valign="center" transparent="1" />
-  <eLabel backgroundColor="#00000000" text="Version: 6.4.21" font="Regular;30" foregroundColor="#00ffffff" position="845,139" size="372,40" halign="center" valign="center" transparent="1" />
+  <eLabel backgroundColor="#00000000" text="Version: 6.4.22" font="Regular;30" foregroundColor="#00ffffff" position="845,139" size="372,40" halign="center" valign="center" transparent="1" />
   <eLabel backgroundColor="#00f0a30a" position="847,208" size="368,2" />
   <eLabel backgroundColor="#00f0a30a" position="847,417" size="368,2" />
   <eLabel backgroundColor="#00f0a30a" position="845,208" size="2,211" />
@@ -1230,7 +1225,7 @@ class KravenVB(ConfigListScreen, Screen):
   </widget>
   <widget backgroundColor="#00000000" name="config" font="Regular;32" foregroundColor="#00ffffff" itemHeight="45" position="105,127" size="1062,810" enableWrapAround="1" scrollbarMode="showOnDemand" transparent="1" zPosition="1" />
   <eLabel backgroundColor="#00000000" text="KravenVB" font="Regular;54" foregroundColor="#00f0a30a" position="1245,120" size="603,69" halign="center" valign="center" transparent="1" />
-  <eLabel backgroundColor="#00000000" text="Version: 6.4.21" font="Regular;45" foregroundColor="#00ffffff" position="1267,208" size="558,60" halign="center" valign="center" transparent="1" />
+  <eLabel backgroundColor="#00000000" text="Version: 6.4.22" font="Regular;45" foregroundColor="#00ffffff" position="1267,208" size="558,60" halign="center" valign="center" transparent="1" />
   <eLabel backgroundColor="#00f0a30a" position="1313,337" size="466,3" />
   <eLabel backgroundColor="#00f0a30a" position="1313,599" size="466,3" />
   <eLabel backgroundColor="#00f0a30a" position="1313,340" size="3,259" />
@@ -1346,10 +1341,6 @@ class KravenVB(ConfigListScreen, Screen):
 		else:
 			emptyLines+=1
 		if self.E2DistroVersion == "VTi":
-			list.append(getConfigListEntry(_("Running Text Renderer"), config.plugins.KravenVB.RunningTextRenderer, _("Choose the version for running text renderer.")))
-		else:
-			emptyLines+=1
-		if self.E2DistroVersion == "VTi":
 			list.append(getConfigListEntry(_("Scrollbars"), config.plugins.KravenVB.ScrollBar, _("Choose the width of scrollbars in lists or deactivate scrollbars completely.")))
 		elif self.E2DistroVersion == "openatv":
 			list.append(getConfigListEntry(_("Scrollbars"), config.plugins.KravenVB.ScrollBar2, _("Choose whether scrollbars should be shown.")))
@@ -1372,7 +1363,7 @@ class KravenVB(ConfigListScreen, Screen):
 				list.append(getConfigListEntry(_("Menu-Transparency"), config.plugins.KravenVB.MenuColorTrans, _("Choose the degree of background transparency for system menu screens.")))
 			else:
 				emptyLines+=1
-			for i in range(emptyLines+3):
+			for i in range(emptyLines+4):
 				list.append(getConfigListEntry(_(" "), ))
 		
 		# page 2
@@ -2117,11 +2108,6 @@ class KravenVB(ConfigListScreen, Screen):
 				self.showText(48,_("runningtext"))
 			elif option.value == "typewriter":
 				self.showText(48,_("typewriter"))
-		elif option == config.plugins.KravenVB.RunningTextRenderer:
-			if option.value == "vti":
-				self.showText(36,_("VRunningText"))
-			elif option.value == "kraven":
-				self.showText(36,_("KravenVBRunningText"))
 		elif option == config.plugins.KravenVB.IBtop:
 			if option.value == "infobar-x2-z1_top":
 				self.showText(50,_("4 Tuner"))
@@ -3553,11 +3539,6 @@ class KravenVB(ConfigListScreen, Screen):
 		self.skinSearchAndReplace.append(['name="KravenLine" value="#00ffffff', 'name="KravenLine" value="#00' + config.plugins.KravenVB.Line.value])
 
 		### Runningtext
-		if self.E2DistroVersion == "VTi":
-			if config.plugins.KravenVB.RunningTextRenderer.value == "vti":
-				self.skinSearchAndReplace.append(["KravenRunningText", "VRunningText"])
-			else:
-				self.skinSearchAndReplace.append(["KravenRunningText", "KravenVBRunningText"])
 		if config.plugins.KravenVB.RunningText.value == "none":
 			self.skinSearchAndReplace.append(["movetype=running", "movetype=none"])
 		if not config.plugins.KravenVB.RunningText.value == "none":
