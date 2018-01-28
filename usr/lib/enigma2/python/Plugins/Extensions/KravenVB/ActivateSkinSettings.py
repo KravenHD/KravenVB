e9382f26c88c1b1c92bbd7ab045c9e276aa8b303
örlgrey <ochzoetna@gmail.com>
2018-01-28 19:56:18 +0100

    Update to Version 6.5.3

--- usr/lib/enigma2/python/Plugins/Extensions/KravenVB/ActivateSkinSettings.py
+++ usr/lib/enigma2/python/Plugins/Extensions/KravenVB/ActivateSkinSettings.py
@@ -473,11 +473,6 @@ config.plugins.KravenVB.ChannelSelectionHorStyle = ConfigSelection(default="csho
 				("cshor-minitv", _("MiniTV"))
 				])
 
-config.plugins.KravenVB.ChannellistPicon = ConfigSelection(default="none", choices = [
-				("on", _("on")),
-				("none", _("off"))
-				])
-
 config.plugins.KravenVB.ChannelSelectionStyle = ConfigSelection(default="channelselection-style-minitv", choices = [
 				("channelselection-style-nopicon", _("no Picon")),
 				("channelselection-style-nopicon2", _("no Picon2")),
@@ -2215,12 +2210,6 @@ class ActivateSkinSettings:
 				if not self.silent:
 					config.usage.servicelist_alternative_mode.value = True
 					config.usage.servicelist_alternative_mode.save()
-			if not self.silent and config.plugins.KravenVB.ChannellistPicon.value == "on":
-				config.usage.servicelist_show_picon.value = "1"
-				config.usage.servicelist_show_picon.save()
-			elif not self.silent:
-				config.usage.servicelist_show_picon.value = False
-				config.usage.servicelist_show_picon.save()
 		
 		### ChannelSelection - openatv
 		elif self.E2DistroVersion == "openatv":
