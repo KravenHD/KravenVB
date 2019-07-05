6c493b6731f0f2f16436e58d3b691e462dcb9951
örlgrey <ochzoetna@gmail.com>
2019-07-05 18:16:00 +0200

    fix Infobar-Channelname

--- usr/lib/enigma2/python/Plugins/Extensions/KravenVB/KravenVB.py
+++ usr/lib/enigma2/python/Plugins/Extensions/KravenVB/KravenVB.py
@@ -4428,15 +4428,15 @@ class KravenVB(ConfigListScreen, Screen):
 			self.skinSearchAndReplace.append(['<!-- Infobar clockstyle -->','<constant-widget name="' + self.actClockstyle + '4"/>'])
 
 		### Infobar Channelname
-		if config.plugins.KravenVB.InfobarStyle.value in ("infobar-style-nopicon","infobar-style-x1"):
+		if config.plugins.KravenVB.InfobarStyle.value in ("infobar-style-nopicon","infobar-style-x1") and not config.plugins.KravenVB.InfobarChannelName.value == "none":
 			self.skinSearchAndReplace.append(['<!-- Infobar channelname -->','<constant-widget name="' + config.plugins.KravenVB.InfobarChannelName.value + '-' + config.plugins.KravenVB.InfobarStyle.value + '"/>'])
-		elif config.plugins.KravenVB.InfobarStyle.value in ("infobar-style-x2","infobar-style-x3","infobar-style-x4","infobar-style-z1","infobar-style-z2"):
+		elif config.plugins.KravenVB.InfobarStyle.value in ("infobar-style-x2","infobar-style-x3","infobar-style-x4","infobar-style-z1","infobar-style-z2") and not config.plugins.KravenVB.InfobarChannelName.value == "none":
 			self.skinSearchAndReplace.append(['<!-- Infobar channelname -->','<constant-widget name="' + config.plugins.KravenVB.InfobarChannelName.value + '-infobar-style-x2"/>'])
-		elif config.plugins.KravenVB.InfobarStyle.value in ("infobar-style-zz1","infobar-style-zz4"):
+		elif config.plugins.KravenVB.InfobarStyle.value in ("infobar-style-zz1","infobar-style-zz4") and not config.plugins.KravenVB.InfobarChannelName.value == "none":
 			self.skinSearchAndReplace.append(['<!-- Infobar channelname -->','<constant-widget name="' + config.plugins.KravenVB.InfobarChannelName.value + '-infobar-style-zz1"/>'])
-		elif config.plugins.KravenVB.InfobarStyle.value in ("infobar-style-zz2","infobar-style-zz3"):
+		elif config.plugins.KravenVB.InfobarStyle.value in ("infobar-style-zz2","infobar-style-zz3") and not config.plugins.KravenVB.InfobarChannelName2.value == "none":
 			self.skinSearchAndReplace.append(['<!-- Infobar channelname -->','<constant-widget name="' + config.plugins.KravenVB.InfobarChannelName2.value + '-infobar-style-zz2"/>'])
-		elif config.plugins.KravenVB.InfobarStyle.value in ("infobar-style-zzz1","infobar-style-zzz2"):
+		elif config.plugins.KravenVB.InfobarStyle.value in ("infobar-style-zzz1","infobar-style-zzz2") and not config.plugins.KravenVB.InfobarChannelName2.value == "none":
 			self.skinSearchAndReplace.append(['<!-- Infobar channelname -->','<constant-widget name="' + config.plugins.KravenVB.InfobarChannelName2.value + '-' + config.plugins.KravenVB.InfobarStyle.value + '"/>'])
 
 		### Infobar/SIB - ecm-info
